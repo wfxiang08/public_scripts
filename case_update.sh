c92f19f0b68f6c43aba1553984a2ab3c28db7ea7
feiwang <wangfei@chunyu.me>
2014-11-10 10:26:41 +0800

    调整case更新的脚本

--- case_update.sh
+++ case_update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+smart_update.py -f Django==1.3.4-dev
+# 或者:
+# ENV/bin/smart_update.py -f Django==1.3.4-dev
+
 wget http://git.chunyu.mobi/python/public_scripts/raw/master/case.py
 sudo cp case.py /System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.py
 sudo rm /System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/unittest/case.pyc
